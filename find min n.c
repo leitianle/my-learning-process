7c45d5735c75b37ad24934e48c3b6fda2e13e354
Leo <2025110528@stu.sufe.edu.cn>
2026-03-24 23:20:01 +0800

    Add files via upload

---
+++ find min n.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+bool is_square(long long x)//judge whether is a square of an integer
+{
+    long long root=(long long)sqrt(x);
+    return(root * root == x);
+}
+
+bool is_cube(long long x)//judge whether is a cube of a integer
+{
+    long long root=(long long)round(pow(x,1.0/3.0));
+    return(root * root * root == x);
+}
+
+int main()
+{
+    long long n = 1;
+    while (true)
+    {
+        if (n%2==0 && n%3==0)
+        {
+            if(is_cube(n/3) && is_square(n/2))//6|n and is_cube and is_square
+            {
+                printf("最小的整数n是%lld\n",n);
+                break;
+            }
+        }
+        
+        n=n+1;
+        
+    }
+    return 0;
+    
+}
